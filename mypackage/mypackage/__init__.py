d7d8588cfa85ef34bc5aa3d0e3d838ddd7fe751e
Anugrahfitri-code <irafitrinovanda@gmail.com>
2024-09-18 14:38:48 +0800

    Delete mypackage directory

--- mypackage/mypackage/__init__.py
+++
@@ -1,3 +0,0 @@
-from module1 import welcome_page
-
-sapaan = welcome_page("selamat datang di permainan kami")
--- mypackage/mypackage/module1.py
+++
@@ -1,5 +0,0 @@
-def welcome_page(title):
-    isian = "="*(len(title)+6)
-    print(f"{isian}")
-    print(f"==={title}===")
-    print(f"{isian}")
